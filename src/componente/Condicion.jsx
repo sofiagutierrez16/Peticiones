3b154fc13a71ee1d092d7c2fca06474c427efd5a
Sofia <gutierrez.sofiamaite@iesconcepcion.edu.ar>
2025-02-26 20:58:01 -0300

    componente condicion

---
+++ src/componente/Condicion.jsx
@@ -0,0 +1,13 @@
+export const Condicion = ({age}) => {
+
+    if (age < 18){
+        alert ('es menor de edad')
+    }
+    else {
+        alert ('es mayor de edad')
+    }
+    return <>
+    
+    </>
+
+}
